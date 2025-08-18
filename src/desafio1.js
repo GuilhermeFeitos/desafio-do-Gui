6b778231b36131a959d02537dd550358cdc1447f
Guins <guifeitos@gmail.com>
2025-08-18 00:30:55 -0300

    exercicio desafio1

---
+++ src/desafio1.js
@@ -0,0 +1,10 @@
+let nomeHeroi ="Guins"
+let xpHeroi 
+
+xpHeroi = 1000
+
+if (xpHeroi <= 1000) {
+    console.log("Ferro")
+}
+
+console.log("O Herói de nome" + (nomeHeroi) + "está no nível de " (xpHeroi))
