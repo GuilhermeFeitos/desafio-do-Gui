efa2e379eefb71dbac79353b99bf41531f7336a2
Guins <guifeitos@gmail.com>
2025-08-18 03:11:04 -0300

    refatorando desafio 1.2

--- src/desafio1.1.js
+++ src/desafio1.1.js
@@ -1,3 +1,5 @@
+
+
 let heroName ="Guins";
 let xpHero = 7050;
 
---
+++ src/desafio1.2.js
@@ -0,0 +1,23 @@
+let nameHero = "Guins";
+let xpHero = 9250;
+let nivel;
+
+if (xpHero === 0 || xpHero <= 1000){
+   nivel = "está no nível Ferro"
+} else if (xpHero === 1001 || xpHero <= 2000 ){
+    nivel = "está no nível Bronze"
+} else if (xpHero === 2001 || xpHero <= 5000){
+    nivel = "está no nível Prata"
+} else if (xpHero === 5001 || xpHero <= 7000){
+    nivel = "está no nível Ouro"
+} else if (xpHero === 7001 || xpHero <= 8000){
+    nivel = "está no nível Platina"
+} else if (xpHero === 8001 || xpHero <= 9000 ){
+    nivel = "está no nível Ascendente"
+} else if (xpHero === 9001 || xpHero <= 10000){
+    nivel = "está no nível Imortal"
+} else {
+    nivel = "está no nível Radiante"
+}
+
+console.log(`O Herói de nome ${nameHero} ${nivel}`)
