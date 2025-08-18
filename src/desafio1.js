fe3f5acb70fabbf8cba3b20ea6a394ed3df190df
Guins <guifeitos@gmail.com>
2025-08-18 01:37:29 -0300

    desafio 1

--- src/desafio1.js
+++ src/desafio1.js
@@ -1,11 +1,26 @@
 let nomeHeroi = "Guins"
-let xpHeroi 
+let xpHeroi
 
-xpHeroi = 1000
-
-if (xpHeroi) {
-    xpHeroi <= 1000
-    console.log("Ferro")
+function validaNivelHeroi(xpHeroi){
+    if(xpHeroi <= 1000){
+        return "Ferro"
+    } else if (xpHeroi === 1001 || xpHeroi <= 2000){
+        return "Bronze"
+    } else if (xpHeroi === 2001 || xpHeroi <= 5000){
+        return "Prata"
+    } else if (xpHeroi === 5001 || xpHeroi <= 6000){
+        return "Ouro"
+    } else if (xpHeroi === 7001 || xpHeroi <= 8000){
+        return "Platina"
+    } else if (xpHeroi === 8001 || xpHeroi <= 9000){ 
+        return "Ascendente"
+    } else if (xpHeroi === 9001 || xpHeroi <= 10000){
+        return "Imortal"
+    } else{
+        return "Radiante"
+    }
 }
 
-console.log("O Herói de nome " + nomeHeroi )
+let res = validaNivelHeroi(1000)
+
+console.log("O Herói de nome " + (nomeHeroi) + " está no nível " + res)
